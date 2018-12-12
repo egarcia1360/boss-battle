d208db9c9d32a1258499e450d3acbecca38514b4
egarcia1360 <43319770+egarcia1360@users.noreply.github.com>
2018-12-12 16:44:32 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,52 @@
 #include <cstring>
 using namespace std;
 
+class Character {
+private:
+	string name;
+	int hp;
+	int shield;
+	bool isStunned;
+
+public:
+	Character(string username, unsigned int health, unsigned int armor) {
+		name = username;
+		hp = health;
+		shield = armor;
+	}
+
+	void attack(Character target, Weapon strike) {
+		if (!isStunned)
+			strike.hit(target);
+		else
+			cout << name << " is stunned!" << endl;
+	}
+
+	unsigned int getHealth() {
+		return hp;
+	}
+
+	string getName() {
+		return name;
+	}
+
+	unsigned int getShield() {
+		return shield;
+	}
+
+	void modifyHealth(int num) {
+		hp += num;
+	}
+
+	void modifyShield(int num) {
+		shield += num;
+	}
+
+	void stun() {
+		isStunned = true;
+	}
+};
+
 class Weapon {
 private:
 	string name;
@@ -25,7 +71,7 @@ public:
 	void hit(Character target) {
 		if (accuracy > rand() % 100) {//Determine if attack hits target
 			bool crit = critChance > rand() % 100;
-			unsigned int attackDamage = crit ? baseDamage : baseDamage * abs(critMult);//Determine base damage
+			unsigned int attackDamage = crit ? baseDamage : (unsigned int)(baseDamage * abs(critMult));//Determine base damage
 			if (crit)
 				cout << "Critical hit!";
 			if (attackDamage > target.getShield()) {//Determine if shield is empty or attack would make shield empty
@@ -42,134 +88,42 @@ public:
 	}
 };
 
-class Character {
-private:
-	string name;
-	unsigned int hp;
-	unsigned int shield;
-	bool isStunned;
-
-public:
-	Character(string username, unsigned int health, unsigned int armor) {
-		name = username;
-		hp = health;
-		shield = armor;
-	}
+void setDifficulty(int level) {
 	
-	void attack(Character target, Weapon strike) {
-		if(!isStunned)
-			strike.hit(target);
-	}
+}
 
-	unsigned int getHealth() {
-		return hp;
+void viewDifficulty(int level) {
+	switch (level) {
+	case 5:
+		cout << "Realistic: Starting Health = XX, " << endl;
+		cout << "Main Weapon = Fists (base damage = XXX and shield health = XXX), Secondary Weapon = (base damage = XXX)" << endl;
+		cout << "Healing items = X Large Potions, X Medium Potions, X Maximum Potion (Large heals XXX, Medium heals XXX, Maximum fills health)" << endl;
+		break;
+	case 4:
+		cout << "Insane: Starting Health = XXX, " << endl;
+		cout << "Main Weapon = base damage = XXX and shield health = XXX), Secondary Weapon = Light Bow(base damage = XXX)" << endl;
+		cout << "Healing items = X Large Potions, X Medium Potions, X Maximum Potion (Large heals XXX, Medium heals XXX, Maximum fills health)" << endl;
+		break;
+	case 3:
+		cout << "Hard: Starting Health = XXX, " << endl;
+		cout << "Main Weapon = Battle Axe(base damage = XXX and shield health = XXX) Secondary Weapon = Long Bow(base damage = XXX), " << endl;
+		cout << "Healing items = X Large Potions, X Medium Potions, X Maximum Potion (Large heals XXX, Medium heals XXX, Maximum fills health)" << endl;
+		break;
+	case 2:
+		cout << "Normal: Starting Health = XXX, " << endl;
+		cout << "Main Weapon = Sword and Shield (base damage = XXX and shield health = XXX), Secondary Weapon = Light Bow(base damage = XXX), " << endl;
+		cout << "Healing items = X Large Potions, X Medium Potions, X Maximum Potion (Large heals XXX, Medium heals XXX, Maximum fills health)" << endl;
+		break;
+	default:
+		cout << "Easy: Starting health = XXX, " << endl;
+		cout << "Main Weapon = Legendary Sword and Shield (base damage = XXX and shield health = XXX), Secondary Weapon = Long Bow(base damage = XXX), " << endl;
+		cout << "Healing items = X Large Potions, X Medium Potions, X Maximum Potion (Large heals XXX, Medium heals XXX, Maximum fills health)" << endl;
+		break;
 	}
-
-	string getName() {
-		return name;
-	}
-	
-	unsigned int getShield() {
-		return shield;
-	}
-	
-	void modifyHealth(int num) {
-		hp += num;
-	}
-
-	void modifyShield(int num) {
-		shield += num;
-	}
-
-	void stun() {
-		isStunned = true;
-	}
-};
-
-void easy() {
-	char x;
-
-	cout << "Easy: Starting health = XXX, " << endl;
-	cout << "Main Weapon = Legendary Sword and Shield (base damage = XXX and shield health = XXX), Secondary Weapon = Long Bow(base damage = XXX), " << endl;
-	cout << "Healing items = X Large Potions, X Medium Potions, X Maximum Potion (Large heals XXX, Medium heals XXX, Maximum fills health)" << endl;
-	cout << "Continue with this mode? Y/N" << endl;
-	cin >> x;
-
-	if (x == 'Y' || x == 'y' || x == 'yes') {
-		cout << "Starting Easy" << endl;
-	}
-	else if (x== 'N' || x== 'n'){
-		cout << "what do you want to play then?" << endl;
-	}
-};
-void normal() {
-	char x;
-
-	cout << "Normal: Starting Health = XXX, " << endl;
-	cout << "Main Weapon = Sword and Shield (base damage = XXX and shield health = XXX), Secondary Weapon = Light Bow(base damage = XXX), " << endl;
-	cout << "Healing items = X Large Potions, X Medium Potions, X Maximum Potion (Large heals XXX, Medium heals XXX, Maximum fills health)" << endl;
-	cout << "Continue with this mode? Y/N" << endl;
-	cin >> x;
-
-	if (x == 'Y' || x == 'y' || x == 'yes') {
-		cout << "Starting Normal" << endl;
-	}
-	else if (x == 'N' || x == 'n') {
-		cout << "what do you want to play then?" << endl;
-	}
-};
-void hard() {
-	char x;
-
-	cout << "Hard: Starting Health = XXX, " << endl;
-	cout << "Main Weapon = Battle Axe(base damage = XXX and shield health = XXX) Secondary Weapon = Long Bow(base damage = XXX), " << endl;
-	cout << "Healing items = X Large Potions, X Medium Potions, X Maximum Potion (Large heals XXX, Medium heals XXX, Maximum fills health)" << endl;
-	cout << "Continue with this mode? Y/N" << endl;
-	cin >> x;
-
-	if (x == 'Y' || x == 'y' || x == 'yes') {
-		cout << "Starting Hard" << endl;
-	}
-	else if (x == 'N' || x == 'n') {
-		cout << "what do you want to play then?" << endl;
-	}
-};
-void insane() {
-	char x;
-
-	cout << "Insane: Starting Health = XXX, " << endl;
-	cout << "Main Weapon = base damage = XXX and shield health = XXX), Secondary Weapon = Light Bow(base damage = XXX)" << endl;
-	cout << "Healing items = X Large Potions, X Medium Potions, X Maximum Potion (Large heals XXX, Medium heals XXX, Maximum fills health)" << endl;
-	cout << "Continue with this mode? Y/N" << endl;
-	cin >> x;
-
-	if (x == 'Y' || x == 'y' || x == 'yes') {
-		cout << "Starting Insane" << endl;
-	}
-	else if (x == 'N' || x == 'n') {
-		cout << "what do you want to play then?" << endl;
-	}
-};
-void realistic() {
-	char x;
-
-	cout << "Realistic: Starting Health = XX, " << endl;
-	cout << "Main Weapon = Fists (base damage = XXX and shield health = XXX), Secondary Weapon = (base damage = XXX)" << endl;
-	cout << "Healing items = X Large Potions, X Medium Potions, X Maximum Potion (Large heals XXX, Medium heals XXX, Maximum fills health)" << endl;
-	cout << "Continue with this mode? Y/N" << endl;
-	cin >> x;
-
-	if (x == 'Y' || x == 'y' || x == 'yes') {
-		cout << "Starting Realistic" << endl;
-	}
-	else if (x == 'N' || x == 'n') {
-		cout << "what do you want to play then?" << endl;
-	}
-};
+}
 
 int main() {
-	int x;
-	int y;
+	int x = 0;
 
 	cout << "Hello and Welcome to ... " << endl;
 	cout << "Within this game you will be set against a random opponent based off the type of challenge you want" << endl;
@@ -180,27 +134,40 @@ int main() {
 	cout << "starting with little to no healing items, and a lower starting health as well" << endl;
 	system("pause");
 
-	cout << "What type of mode would you like to do?" << endl;
-	cout << "1. Easy" << endl;
-	cout << "2. Normal" << endl;
-	cout << "3. Hard" << endl;
-	cout << "4. Insane" << endl;
-	cout << "5. Realistic" << endl;
-	cin >> x;
+	while (x < 1 || x > 5) {
+		cout << "Select your difficulty level:" << endl;
+		cout << "1. Easy" << endl;
+		cout << "2. Normal" << endl;
+		cout << "3. Hard" << endl;
+		cout << "4. Insane" << endl;
+		cout << "5. Realistic" << endl;
+		cin >> x;
 
-	if (x == 1) {
-		easy();
+		viewDifficulty(x);
+
+		string diff;
+		switch (x) {
+		case 1:
+			diff = "easy";
+			break;
+		case 2:
+			diff = "normal";
+			break;
+		case 3:
+			diff = "hard";
+			break;
+		case 4:
+			diff = "insane";
+			break;
+		case 5:
+			diff = "realistic";
+			break;
+		}
+		while (!(x == 'Y' || x == 'y'))
+			cout << "Are you ready to play in " << diff << " mode? [Y/N]" << endl;
+		
+		//THIS NEEDS TO BE CHECKED AND PROBABLY EDITED TO MAKE SURE IT WORKS, AND THEN FILLED IN TO ACTUALLY SET THE DETAILS OF THE DIFFICULTY
+		//LIKE CREATING THE CHARACTERS AND GIVING THEM THE WEAPONS AND ALL THAT
 	}
-	else if (x == 2) {
-		normal();
-	}
-	else if (x == 3) {
-		hard();
-	}
-	else if (x == 4) {
-		insane();
-	}
-	else if (x == 5) {
-		realistic();
-	}
+	
 }
